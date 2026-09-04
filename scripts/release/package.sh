a15a122592b0e2cf8a2aec32e86ac8bd8b28169b
eeee0717 <chentao020717Work@outlook.com>
2026-09-04 19:42:02 +0800

    fix: make release hashing portable on Windows

--- scripts/release/package.sh
+++ scripts/release/package.sh
@@ -23,9 +23,9 @@ mkdir -p "$output_dir"
 
 sha256_file() {
   if command -v sha256sum >/dev/null 2>&1; then
-    sha256sum "$1" | awk '{print $1}'
+    sha256sum < "$1" | awk '{print $1}'
   else
-    shasum -a 256 "$1" | awk '{print $1}'
+    shasum -a 256 < "$1" | awk '{print $1}'
   fi
 }
 
